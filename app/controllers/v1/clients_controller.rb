24fc3cf66e8fa20829065ff4c0d03e2f4133ad43
bgrove1974 <bgrove1974@gmail.com>
2014-11-20 01:41:54 -0800

    Update clients_controller

--- app/controllers/v1/clients_controller.rb
+++ app/controllers/v1/clients_controller.rb
@@ -1,5 +1,23 @@
+require 'civic_aide'
+
 module V1
   class ClientsController < ApplicationController
+    ## Set API key with global configuration so resources can be accessed directly
+    CivicAide.api_key = ENV["GOOGLEAPIKEY"]
+
+    ## Receive data only from official state sources
+    ## CivicAide.official_sources = true
+
+    ## Get info for all elections
+    CivicAide.elections.all
+
+    ## Find election info by address example
+    ## client.election(4015).at('4910 Willet Drive, Annandale, VA 22003')
+
+    ## Retrieve political geography and representative information based on an address example
+    ## client.representatives.at('118 E. Main St. Carrboro, NC 27510')
+
+
 ## Google Civic Information API does not allow GET requests
 ##  POST /clients
 ##  POST /clients.json
