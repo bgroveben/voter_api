e6689e956425e3b6b299c67fa13511c3913220b4
bgrove1974 <bgrove1974@gmail.com>
2014-11-20 02:50:47 -0800

    Delete lib/client.rb

--- app/controllers/v1/clients_controller.rb
+++ app/controllers/v1/clients_controller.rb
@@ -1,3 +1,5 @@
+require 'civic_aide'
+
 module V1
   class ClientsController < ApplicationController
 
@@ -18,3 +20,21 @@ module V1
       end
   end
 end
+
+
+
+
+## Set API key with global configuration so resources can be accessed directly
+## CivicAide.api_key = ENV["GOOGLEAPIKEY"]
+
+## Receive data only from official state sources
+## CivicAide.official_sources = true
+
+## Get info for all elections
+## CivicAide.elections.all
+
+## Find election info by address example
+## client.election(4015).at('4910 Willet Drive, Annandale, VA 22003')
+
+## Retrieve political geography and representative information based on an address example
+## client.representatives.at('118 E. Main St. Carrboro, NC 27510')
